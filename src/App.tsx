344200177bec7ac873705cd78929c74e8fa6285e
Leonardo Piusi <leopidev@gmail.com>
2021-12-15 23:37:09 -0300

    feat: routes config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import Routes from './routes';
+
 const App = () => {
-  return <ChakraProvider />;
+  return (
+    <ChakraProvider>
+      <Routes />
+    </ChakraProvider>
+  );
 };
 
 export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+} from 'react-router-dom';
+
+const Routes: React.FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" element={} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
