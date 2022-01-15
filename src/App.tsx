3686a6f2cb85b1e8facac223c38430ce3c1957f8
Leonardo Piusi <leopidev@gmail.com>
2022-01-15 03:10:39 -0300

    feat: add React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 
-import { ThemeProvider } from './contexts/theme-context';
-import { GlobalStyle } from './styles/global';
 import Routes from './routes';
 
 const App: React.FC = () => {
-  return (
-    <ThemeProvider>
-      <GlobalStyle />
-      <Routes />
-    </ThemeProvider>
-  );
+  return <Routes />;
 };
 
 export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,16 +4,28 @@ import {
   Routes as Switch,
   Route,
 } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import { Register } from '../pages';
+import { ThemeProvider } from '../contexts/theme-context';
+import { GlobalStyle } from '../styles/global';
 
 const Routes: React.FC = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/main_window" element={<Register />} />
-      </Switch>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <ThemeProvider>
+          <GlobalStyle />
+          <ReactQueryDevtools initialIsOpen />
+          <Switch>
+            <Route path="/main_window" element={<Register />} />
+          </Switch>
+        </ThemeProvider>
+      </Router>
+    </QueryClientProvider>
   );
 };
 
