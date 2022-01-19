482df5df97c53a9bf529805ec231f8e4b6bfdbe4
Leonardo Piusi <leopidev@gmail.com>
2022-01-19 02:08:07 -0300

    chore: build config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const Dotenv = require('dotenv-webpack');
 const path = require('path');
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
+  mode: process.env.NODE_ENV,
   entry: path.resolve(__dirname, "src/index.tsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
